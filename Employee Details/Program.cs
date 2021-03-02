36ac07e3f327af9fb6c47492b260d8c91cc7d4d7
amit <[amirana14325@gmail.com]>
2021-03-03 01:16:45 +0530

    [AMIT RANA]Added::welcome massege

---
+++ Employee Details/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Employee_Details
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Welcome To My Console Application For Employee Details Using .NET Framework and C# Language By AMIT RANA");
+            Console.ReadKey();
+        }
+    }
+}
