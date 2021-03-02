19de4baf4ffa4bfeda78a141c9fb8fa609caf13b
amit <[amirana14325@gmail.com]>
2021-03-03 01:21:45 +0530

    [AMIT RANA]Added::employee attendence

--- Employee Details/Program.cs
+++ Employee Details/Program.cs
@@ -6,7 +6,13 @@ namespace Employee_Details
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Welcome To My Console Application For Employee Details Using .NET Framework and C# Language By AMIT RANA");
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+                Console.WriteLine("Employee is present=" + empCheck);
+            else
+                Console.WriteLine("Employee is absent=" + empCheck);
             Console.ReadKey();
         }
     }
