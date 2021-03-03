f3f333f50a7bd772292407334dcfa1da394ffd5a
amit <[amirana14325@gmail.com]>
2021-03-03 09:50:14 +0530

    [AMIT RANA]Added::empolyee wage

--- Employee Details/Program.cs
+++ Employee Details/Program.cs
@@ -6,14 +6,19 @@ namespace Employee_Details
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Employee Wage Computation Problem");
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
-                Console.WriteLine("Employee is present=" + empCheck);
-            else
-                Console.WriteLine("Employee is absent=" + empCheck);
-            Console.ReadKey();
+            {
+                empHrs = 8;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wage :" + empWage);
         }
     }
 }
